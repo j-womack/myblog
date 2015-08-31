79c237ff26770cd05383717f56ecc24d4908d261
j-womack <womack.joshua@gmail.com>
2015-08-31 15:48:20 -0500

    added pagedown editor

--- app/views/posts/create-edit-form.blade.php
+++ app/views/posts/create-edit-form.blade.php
@@ -5,22 +5,7 @@
         
 <div class="wmd-panel">
     <div id="wmd-button-bar"></div>
-    <textarea class="wmd-input form-control" name="body" cols="50" rows="10" id="wmd-input">
-This is the *second* editor.
-------------------------------
-
-It has a plugin hook registered that surrounds all words starting with the
-letter A with asterisks before doing the Markdown conversion. Another one gives bare links
-a nicer link text. User input isn't sanitized here:
-
-<marquee>I'm the ghost from the past!</marquee>
-
-http://google.com
-
-http://stackoverflow.com
-
-It also includes a help button.
-    </textarea>
+    <textarea class="wmd-input form-control" name="body" cols="50" rows="10" id="wmd-input"></textarea>
 </div>
 <label>Preview:</label>
 <div id="wmd-preview" class="wmd-panel wmd-preview"></div>
