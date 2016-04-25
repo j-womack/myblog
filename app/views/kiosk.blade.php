9b968b78d6cea12fe4d95c6b830476b9c859fb37
j-womack <womack.joshua@gmail.com>
2016-04-25 18:36:11 -0500

    Resized calendar to fit smaller screen

--- app/views/kiosk.blade.php
+++ app/views/kiosk.blade.php
@@ -53,7 +53,7 @@
     <body>
         <div id="map" class="col-lg-6"></div>
         <div id="calendar" class="col-lg-6">
-            <iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showPrint=0&amp;showCalendars=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=noisefights%40gmail.com&amp;color=%232952A3&amp;ctz=America%2FChicago" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+            <iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showPrint=0&amp;showCalendars=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=noisefights%40gmail.com&amp;color=%232952A3&amp;ctz=America%2FChicago" style="border-width:0" width="680" height="600" frameborder="0" scrolling="no"></iframe>
         </div>
         <div id="clock"></div>
         <div id="weatherBox"></div>
